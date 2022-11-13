7a2c528117fffc9d7ce2cbe51276e3b361b72834
do-no-van <47617272696e@gmail.com>
2022-11-13 11:48:50 -0600

    improve lua/ascii_bg.lua

--- lua/ascii_bg.lua
+++ lua/ascii_bg.lua
@@ -4,10 +4,8 @@ local fn = vim.fn
 
 local M = {}
 
-cmd("highlight AsciiBorder    guifg=#02244f")
-cmd("highlight InvBorderAscii guifg=background guibg=#02244f")
-cmd("highlight Ascii          guifg=#02244f guibg=#055ff0")
-cmd("highlight InvAscii       guifg=#055ff0 guibg=#02244f")
+cmd("highlight AsciiBorder    guifg=#280068")
+cmd("highlight Ascii          guifg=#280068 guibg=#3f007f")
 
 local NVIM_VERSION = "NVIM " .. fn.system('nvim --version | head -n 1 | awk \'{print $2}\''):gsub("\n", "")
 
@@ -19,75 +17,34 @@ local function pad_str(padding, string)
     return string
 end
 
--- One block character is not one coordinate to the right, so these work
--- even though they look completely random.
+-- one block character is not one coordinate to the right, so these work
+-- even though they look completely random
 
--- End of left border, start of right border
--- Borders are non-full blocks on the edge.
--- They are done seperatly since they shouldn't
--- change their background color.
--- e.g. ▂▂▄▄<c1>████<c2>▄▄▂▂
+-- end of left border, start of right border
+-- borders are non-full blocks on the edge
+-- they are done seperatly since they shouldn't change their background color
+-- i.e. { <c1>, <c2> } = ▂▂▄▄<c1>████<c2>▄▄▂▂
 local ascii_coords = {
     { 25, 28 },
     { 38, 50 },
     { 22, 67 },
-    { 12, 57 },
+    { 14, 77 },
     { 14, 57 },
-    { 11, 98 },
-    {  8, 96 },
+    { 11, 48 },
+    { 18, 56 },
     {  8, 87 },
     {  8, 79 },
-    {  8, 72 },
-    { 11, 69 },
-    { 14, 61 },
-    { 16, 61 },
+    { 18, 62 },
+    { 11, 59 },
+    { 14, 51 },
+    { 16, 81 },
     { 18, 69 },
-    { 27, 50 },
+    { 37, 50 },
     { 25, 28 },
 }
 
--- Line, end of left border, start of right border
--- Need to invert some blocks because, for example
--- there is a left half block but no right half.
--- Inverting the colors effectivly makes a left half.
-local inv_ascii_coords = {
-    {  4, 28, 28 },
-    {  4, 33, 38 },
-    {  4, 54, 59 },
-    {  4, 64, 64 },
-    {  6, 45, 45 },
-    { 8, 25, 25 },
-    { 8, 79, 79 },
-    { 9, 76, 76 },
-    { 10, 68, 68 },
-    { 11, 28, 28 },
-    { 11, 52, 52 },
-    { 12, 31, 31 },
-    { 14, 35, 35 },
-}
-
--- None of these were next to each other, so
--- ther was no need for the end column field
-local inv_border_ascii_coords = {
-    { 9,  8 },
-    { 10,  8 },
-    { 12,  9 },
-    { 13, 70 },
-    { 14, 12 },
-    { 14, 64 },
-    { 15, 14 },
-    { 15, 17 },
-    { 15, 71 },
-    { 15, 74 },
-    { 16, 20 },
-    { 16, 26 },
-    { 16, 50 },
-    { 16, 56 },
-}
-
 local function color_buf(buf, padding)
-    local start_x, end_x = 6, 100
-    local start_y = 1
+    local start_y = 1 -- newlines before drawing text
     local ns = api.nvim_create_namespace("ascii_ns")
 
     for line, coord in ipairs(ascii_coords) do
@@ -96,7 +53,7 @@ local function color_buf(buf, padding)
             ns,
             "AsciiBorder",
             line + start_y - 1,
-            start_x + padding - 1,
+            0,
             coord[1] + padding
         )
 
@@ -115,33 +72,11 @@ local function color_buf(buf, padding)
             "AsciiBorder",
             line + start_y - 1,
             coord[2] + padding - 1,
-            end_x + padding
+            1000 -- this can be any large number, it just means continue highlighting until this col
         )
     end
 
-    for _, coord in ipairs(inv_border_ascii_coords) do
-        api.nvim_buf_add_highlight(
-            buf,
-            ns,
-            "InvBorderAscii",
-            coord[1] - 1,
-            coord[2] + padding - 1,
-            coord[2] + padding
-        )
-    end
-
-    for _, coord in ipairs(inv_ascii_coords) do
-        api.nvim_buf_add_highlight(
-            buf,
-            ns,
-            "InvAscii",
-            coord[1] - 1,
-            coord[2] + padding - 1,
-            coord[3] + padding
-        )
-    end
-
-    -- Darken instances of the word <Enter>
+    -- darken instances of the word <Enter>
     for _, coord in ipairs({ { 23, 17, 23 }, { 24, 19, 25 }, { 25, 9, 15 }, { 26, 12, 18 } }) do
         api.nvim_buf_add_highlight(
             buf,
@@ -155,23 +90,23 @@ local function color_buf(buf, padding)
 end
 
 local ascii = {
-    "                                          ",
-    "                   ▁▁▁▁▁",
-    "              ▁▃▄▆███████▆▄▃▁",
-    "          ▁▄▅▇█▂▀▆▆▔▔▔▔▔▆▆▀▂█▇▅▄▁",
-    "         ▟███▀▔             ▔▀███▙",
-    "       ▂▟███▌                 ▌███▙▂",
-    "      ▟██████▇▆▆▅▄▃▂▂▂▂▂▃▄▅▆▆▇██████▙",
-    "      ███▛ ▔▅▜███████████████▛▅▔ ▜███",
-    "     ▊███▎    ▝▜███████████▛▘    ▊███▎",
-    "     ▊███▎      ▜█████████▛      ▊███▎",
-    "      ███▙       ▄███████▄       ▟███",
-    "      ▍███▙       ▌█████▌       ▟███▍",
-    "       ▔▜██▙▂      ▜███▛      ▂▟██▛▆",
-    "         ▆▜██▙▂     ▏█▉     ▂▟██▛▆",
-    "           ▆▃███▇▆▅▄▟█▙▄▅▆▇███▃▆",
-    "              ▔▆▀▂███████▂▀▆▔",
-    "                   ▔▔▔▔▔",
+    "                                              ", -- this line needs to be the same length as the
+    "                   ▁▁▁▁▁▁▁",                     -- longest line for padding to work correctly
+    "             ▁▁▄▄▟█████████▙▄▄▁▁",
+    "            ▟███████████████████▙",
+    "          ▄████▛▀▀▀▀▀▀▀▀▀▀▀▀▀▜████▄",
+    "         ▟████▛               ▜████▙",
+    "        ▟████▛                 ▜████▙",
+    "       ▐████▛                   ▜████▌",
+    "       ████▛                     ▜████",
+    "       ████▙                     ▟████",
+    "       ▐████▙                   ▟████▌",
+    "        ▜████▙                 ▟████▛",
+    "         ▜████▙               ▟████▛",
+    "          ▀████▙▄▄▄▄▄▄▄▄▄▄▄▄▄▟████▀",
+    "            ▜███████████████████▛",
+    "             ▔▔▀▀▜█████████▛▀▀▔▔",
+    "                   ▔▔▔▔▔▔▔",
     "__NVIM_VERSION__",
     "",
     " Nvim is open source and freely distributable",
@@ -206,11 +141,11 @@ M["set_ascii_bg"] = function()
     local win = api.nvim_get_current_win()
     local buf = api.nvim_create_buf(true, true)
 
-    -- Only display if there is enough space
+    -- only display if there is enough space
     if height >= ascii_rows and width >= ascii_cols then
         for i, _ in ipairs(ascii) do
             if ascii[i] == "__NVIM_VERSION__" then
-                -- Center version number before padding it
+                -- center version number before padding it
                 ascii[i] = pad_str(pad_cols, pad_str(math.ceil((ascii_cols - #NVIM_VERSION) / 2), NVIM_VERSION))
             elseif ascii[i] ~= "" then
                 ascii[i] = pad_str(pad_cols, ascii[i])
